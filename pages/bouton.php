356f1fb15fd3536e79b692a3451ae6e5b47c8b9e
Mitiafyh <mitiafiorenantsoa@gmail.com>
2025-07-14 16:03:40 +0300

    new

--- pages/bouton.php
+++ pages/bouton.php
@@ -1,9 +1,6 @@
 <?php
 require("../includes/fonction.php");
 $num=$_GET['num'];
-
-
-
 $sql="UPDATE PF_emprunt SET date_retour=(NOW()+3) WHERE id_objet=".$num ;
 $resultat = mysqli_query(bdd(), $sql);
 
